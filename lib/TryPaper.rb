9b761c35bda5662b9bdf0ddbd22a7c584470ec49
Patrick Jones <pmichaeljones@gmail.com>
2015-01-06 12:11:02 -0700

    passing api_key test

--- lib/TryPaper.rb
+++ lib/TryPaper.rb
@@ -3,6 +3,13 @@ require "TryPaper/version"
 module TryPaper
 
   class Mailing
+
+    attr_reader :api_key
+
+    def initialize(api_key)
+      @api_key = api_key
+    end
+
   end
 
 end
--- spec/trypaper_spec.rb
+++ spec/trypaper_spec.rb
@@ -3,13 +3,14 @@ require 'spec_helper'
 describe TryPaper do
 
   it 'should exist' do
-    mailing = TryPaper::Mailing.new
+    api_key = "12345"
+    mailing = TryPaper::Mailing.new(api_key)
     expect(mailing).to be_kind_of(TryPaper::Mailing)
   end
 
   it 'should accept API key' do
     api_key = "12345"
-    mailing = TryPaper::Mailing.new(api_key, api_token)
+    mailing = TryPaper::Mailing.new(api_key)
     expect(mailing.api_key).to eq(api_key)
   end
 
