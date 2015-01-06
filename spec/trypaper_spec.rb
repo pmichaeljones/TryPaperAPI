8cd7a97f4a05d75fd9964667a5346e6b9eed6641
Patrick Jones <pmichaeljones@gmail.com>
2015-01-06 12:06:09 -0700

    API key test

--- spec/trypaper_spec.rb
+++ spec/trypaper_spec.rb
@@ -7,4 +7,10 @@ describe TryPaper do
     expect(mailing).to be_kind_of(TryPaper::Mailing)
   end
 
+  it 'should accept API key' do
+    api_key = "12345"
+    mailing = TryPaper::Mailing.new(api_key, api_token)
+    expect(mailing.api_key).to eq(api_key)
+  end
+
 end
