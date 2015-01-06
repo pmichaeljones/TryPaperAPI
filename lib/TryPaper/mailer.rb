3e5f61036845720dfc60f63c79bba7ea6c092e60
Patrick Jones <pmichaeljones@gmail.com>
2015-01-06 14:14:58 -0700

    properly organized gem files

---
+++ lib/TryPaper/mailer.rb
@@ -0,0 +1,17 @@
+module TryPaper
+
+  #the Mailer class takes an instance of the document class and sends it to the TryPaper API
+  class Mailer
+
+    attr_reader :api_key, :document
+
+    def initialize(api_key, document = nil)
+      @api_key = api_key
+      @document = document
+    end
+
+
+
+  end
+
+end
---
+++ spec/document_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe TryPaper::Document do
+
+  it 'should exist' do
+    doc = TryPaper::Document.new
+    expect(doc).to be_kind_of(TryPaper::Document)
+  end
+
+  it 'should accept a file when initialized' do
+    ##
+  end
+
+  it 'should read the file' do
+    ##
+  end
+
+  it 'should base64 encode the file and store in a variable' do
+    ##
+  end
+
+end
