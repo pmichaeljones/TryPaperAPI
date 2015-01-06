ae06b086c884f0079bcd091bce3b70e48da94552
Patrick Jones <pmichaeljones@gmail.com>
2015-01-06 13:48:06 -0700

    added document to init method

--- lib/TryPaper.rb
+++ lib/TryPaper.rb
@@ -5,11 +5,14 @@ module TryPaper
   #the Mailer class takes an instance of the document class and sends it to the TryPaper API
   class Mailer
 
-    attr_reader :api_key
+    attr_reader :api_key, :document
 
-    def initialize(api_key)
+    def initialize(api_key, document = nil)
       @api_key = api_key
+      @document = document
     end
+
+
 
   end
 
--- spec/mailer_spec.rb
+++ spec/mailer_spec.rb
@@ -14,5 +14,19 @@ describe TryPaper::Mailer do
     expect(mailing.api_key).to eq(api_key)
   end
 
+  it 'should accept a document when given' do
+    api_key = "12345"
+    document = "ENCODED-DOCUMENT"
+    mailing = TryPaper::Mailer.new(api_key, document)
+    expect(mailing.document).to eq(document)
+  end
+
+  it 'should set document to nil when not given' do
+    api_key = "12345"
+    mailing = TryPaper::Mailer.new(api_key)
+    expect(mailing.document).to eq(nil)
+  end
+
+
 end
 
