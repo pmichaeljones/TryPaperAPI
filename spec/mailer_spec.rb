c778bea091ef1dc124b1aa17e30b082d027548b9
Patrick Jones <pmichaeljones@gmail.com>
2015-01-22 20:28:56 -0700

    refactor tests

--- spec/mailer_spec.rb
+++ spec/mailer_spec.rb
@@ -3,8 +3,13 @@ require 'spec_helper'
 # Testing the class here
 describe TryPaper::Mailer do
 
+  let(:mailing){TryPaper::Mailer.new(TEST_API_KEY, RETURN_ADDRESS_ID)}
+
+  before do
+
+  end
+
   it 'should exist' do
-    mailing = TryPaper::Mailer.new(TEST_API_KEY, RETURN_ADDRESS_ID)
     expect(mailing).to be_kind_of(TryPaper::Mailer)
   end
 
@@ -12,7 +17,6 @@ describe TryPaper::Mailer do
     file = File.read('../../Documents/mac.pdf')
     doc = TryPaper::Document.new(file)
     recipient = TryPaper::Recipient.new("Patrick Jones", "555 Main Street", "Apt 1", "Denver", "CO", "54323")
-    mailing = TryPaper::Mailer.new(TEST_API_KEY, RETURN_ADDRESS_ID)
     mailing.recipient = recipient
     expect(mailing.recipient).to eq(recipient)
   end
@@ -20,7 +24,6 @@ describe TryPaper::Mailer do
   it 'should have a document setter method' do
     file = File.read('../../Documents/mac.pdf')
     doc = TryPaper::Document.new(file)
-    mailing = TryPaper::Mailer.new(TEST_API_KEY, RETURN_ADDRESS_ID)
     mailing.document = doc
     expect(mailing.document).to eq(doc)
   end
@@ -29,11 +32,6 @@ describe TryPaper::Mailer do
     mailing = TryPaper::Mailer.new(TEST_API_KEY, RETURN_ADDRESS_ID, ["triplicate", "duplicate"])
     expect(mailing.tags).to eq(["triplicate", "duplicate"])
   end
-
-end
-
-# Testing the API calls here
-describe TryPaper::Mailer do
 
   context 'with a correct(pdf) document type' do
     file = File.read('../../Documents/mac.pdf')
@@ -53,9 +51,6 @@ describe TryPaper::Mailer do
 
   end
 
-end
-
-describe TryPaper::Mailer do
   context 'with wrong document type' do
     file = File.read('./spec/documents/input_document.txt')
     doc = TryPaper::Document.new(file)
--- spec/recipient_spec.rb
+++ spec/recipient_spec.rb
@@ -2,43 +2,38 @@ require 'spec_helper'
 
 describe TryPaper::Recipient do
 
+  let(:receiver){TryPaper::Recipient.new}
+
   it 'should exist' do
-    receiver = TryPaper::Recipient.new
     expect(receiver).to be_kind_of(TryPaper::Recipient)
   end
 
   it 'should have a name setter method' do
-    receiver = TryPaper::Recipient.new
     receiver.name = "Patrick"
     expect(receiver.name).to eq("Patrick")
   end
 
   it 'should have a address1 setter method' do
-    receiver = TryPaper::Recipient.new
     receiver.address1 = "555 Main Street"
     expect(receiver.address1).to eq("555 Main Street")
   end
 
   it 'should have a address2 setter method' do
-    receiver = TryPaper::Recipient.new
     receiver.address2 = "Apt 2"
     expect(receiver.address2).to eq("Apt 2")
   end
 
   it 'should have a city setter method' do
-    receiver = TryPaper::Recipient.new
     receiver.city = "Denver"
     expect(receiver.city).to eq("Denver")
   end
 
   it 'should have a state setter method' do
-    receiver = TryPaper::Recipient.new
     receiver.state = "Wisconsin"
     expect(receiver.state).to eq("Wisconsin")
   end
 
   it 'should have a zipcode setter method' do
-    receiver = TryPaper::Recipient.new
     receiver.zipcode = 342353
     expect(receiver.zipcode).to eq(342353)
   end
@@ -54,6 +49,5 @@ describe TryPaper::Recipient do
       receiver = TryPaper::Recipient.new
       expect(receiver.formatted_address).to be_kind_of(Hash)
     end
-
   end
 end
